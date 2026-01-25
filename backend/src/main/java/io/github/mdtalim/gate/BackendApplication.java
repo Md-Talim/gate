5c3693ea8d5e499f284ee0ca5893f0192a90ad5d
Md. Talim <talimmohammad116@gmail.com>
2026-01-25 15:54:06 +0530

    Initialize backend project in spring boot

---
+++ backend/src/main/java/io/github/mdtalim/gate/BackendApplication.java
@@ -0,0 +1,23 @@
+package io.github.mdtalim.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BackendApplication {
+    final private String value = "name";
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = value, defaultValue = "Talim") String name) {
+        return String.format("hello, %s", name);
+    }
+
+}
