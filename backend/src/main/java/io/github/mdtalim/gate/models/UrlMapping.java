70e7506882e906187b0ca687c61f7b076625d979
Md. Talim <talimmohammad116@gmail.com>
2026-01-25 17:20:53 +0530

    Add `ClickEvent`, `UrlMapping`, and `User` entity classes for backend model

---
+++ backend/src/main/java/io/github/mdtalim/gate/models/UrlMapping.java
@@ -0,0 +1,27 @@
+package io.github.mdtalim.gate.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String longUrl;
+    private String shortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEventList;
+}
